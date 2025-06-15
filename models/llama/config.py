90ded8d3c247eeeeee71a483eded49d6a4e9cf1d
Ammar <ammar3.shaikh@t1v-n-26ae921b-w-0.us-central2-b.c.gtrc-462712.internal>
2025-06-15 07:47:01 +0000

    verified forward pass for LLaMa -> memory req for [1,16] -> 13GB

--- models/llama/config.py
+++ models/llama/config.py
@@ -75,6 +75,6 @@ class ModelConfig:
             vocab_size=hf_config['vocab_size'],
             rms_norm_eps=hf_config['rms_norm_eps'],
             rope_theta=hf_config.get('rope_theta', 10000.0),
-            max_seq_len=hf_config.get('max_position_embeddings', 2048),
+            max_seq_len=hf_config['rope_scaling']['original_max_position_embeddings'],
             activation_fn=hf_config.get('hidden_act', 'silu')
         )
